fd1100311b0b3761b45157b73a6c60ff2e40333b
Mitchell Stanton <mitch@syntima.dev>
2025-07-15 17:28:57 -0400

    Add Protobuf definitions and generation script for Dislo service

---
+++ proto/proto.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ $protoFile == "" ]]; then
+  protoFile="dislo.proto"
+fi
+
+# Make sure script is run from the proto directory
+currentDir=${PWD##*/}
+if [ $currentDir != "proto" ]; then
+  echo "Please run this script from the proto directory"
+  exit 1
+fi
+
+appDir="../app/pkg"
+
+rm -rf $appDir/generated
+mkdir -p $appDir
+
+# Generate code for all targets
+protoc \
+  -I. \
+  --go_out=$appDir \
+  --go-grpc_out=$appDir \
+  --proto_path=. \
+  $protoFile || exit 1
+
+# Generate descriptor specifically for grpcurl
+protoc \
+  --proto_path=. \
+  --include_imports \
+  --include_source_info \
+  -o services.desc \
+  $protoFile || exit 1
